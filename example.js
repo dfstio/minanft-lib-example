fdae2e7eab69488aa03bacd0f0b42d7bf2180039
DFST <mk@dfst.io>
2023-11-15 23:18:02 +0400

    update example

--- example.js
+++ example.js
@@ -1,23 +1,95 @@
-const { MinaNFT } = require("minanft");
+const  {
+  MinaNFT,
+  MapData,
+  MinaNFTNameService,
+  MINANFT_NAME_SERVICE,
+  accountBalanceMina,
+} = require("minanft");
+const  { PrivateKey, PublicKey, Poseidon } = require("o1js");
+const {  DEPLOYER, PINATA_JWT, NAMES_ORACLE_SK } = require("./env.json");
 
 async function main() {
-    await MinaNFT.minaInit();
-    const nft = new MinaNFT();
+  const keys = MinaNFT.minaInit('testworld2');
+  const deployer = keys ? keys[0].privateKey : PrivateKey.fromBase58(DEPLOYER);
+  const oraclePrivateKey = PrivateKey.fromBase58(NAMES_ORACLE_SK);
+  const nameServiceAddress = PublicKey.fromBase58(MINANFT_NAME_SERVICE);
 
-    nft.publicData.set("name", "@test");
-    nft.publicData.set("description", "my nft @test");
-    nft.publicData.set("image", "https/ipfs.io/ipfs/Qm...");
+  const ownerPrivateKey = PrivateKey.random();
+  const ownerPublicKey = ownerPrivateKey.toPublicKey();
+  const owner = Poseidon.hash(ownerPublicKey.toFields());
+  const pinataJWT = PINATA_JWT;
 
-    nft.privateData.set("name", "cohort2");
+  console.log(
+    `Deployer balance: ${await accountBalanceMina(deployer.toPublicKey())}`
+  );
 
-    const publicJson = await nft.getPublicJson();
-    console.log("publicJson", publicJson);
+  const nft = new MinaNFT(`@test`);
+  nft.updateText({
+    key: `description`,
+    text: "This is my long description of the NFT. Can be of any length, supports markdown.",
+  });
+  nft.update({ key: `twitter`, value: `@builder` });
+  nft.update({ key: `secret`, value: `mysecretvalue`, isPrivate: true });
 
-    const privateJson = await nft.getPrivateJson();
-    console.log("privateJson", privateJson);
+  await nft.updateImage({
+    filename: "./images/navigator.jpg",
+    pinataJWT,
+  });
+
+  const map = new MapData();
+  map.update({ key: `level2-1`, value: `value21` });
+  map.update({ key: `level2-2`, value: `value22` });
+  map.updateText({
+    key: `level2-3`,
+    text: `This is text on level 2. Can be very long`,
+  });
+
+  await map.updateFile({
+    key: "woman",
+    filename: "./images/woman.png",
+    pinataJWT,
+  });
+
+  const mapLevel3 = new MapData();
+  mapLevel3.update({ key: `level3-1`, value: `value31` });
+  mapLevel3.update({ key: `level3-2`, value: `value32`, isPrivate: true });
+  mapLevel3.update({ key: `level3-3`, value: `value33` });
+  map.updateMap({ key: `level2-4`, map: mapLevel3 });
+  nft.updateMap({ key: `level 2 and 3 data`, map });
+
+  console.log(`json:`, JSON.stringify(nft.toJSON(), null, 2));
+  console.log("Compiling...");
+  await MinaNFT.compile();
+
+  /*
+  const nameService = new MinaNFTNameService({ oraclePrivateKey });
+  let tx = await nameService.deploy(deployer);
+  if (tx === undefined) {
+    throw new Error("Deploy failed");
+  }
+  await MinaNFT.wait(tx);
+  */
+  
+  const nameService = new MinaNFTNameService({
+    oraclePrivateKey,
+    address: nameServiceAddress,
+  });
+  
+  const tx = await nft.mint({
+    deployer,
+    owner,
+    pinataJWT,
+    nameService,
+  });
+  if (tx === undefined) {
+    throw new Error("Mint failed");
+  }
+  console.log("Waiting for transaction to be included in a block...");
+  console.time("Transaction included in a block");
+  await MinaNFT.wait(tx);
+  console.timeEnd("Transaction included in a block");
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
 });
--- example.ts
+++ example.ts
@@ -1,61 +1,95 @@
-import { MinaNFT, MapData } from "minanft";
-import { PrivateKey, Poseidon } from "o1js";
-import { PINATA_JWT, DEPLOYER } from "./env.json";
-
+import {
+  MinaNFT,
+  MapData,
+  MinaNFTNameService,
+  MINANFT_NAME_SERVICE,
+  accountBalanceMina,
+} from "minanft";
+import { PrivateKey, PublicKey, Poseidon } from "o1js";
+import { PINATA_JWT, DEPLOYER, NAMES_ORACLE_SK } from "./env.json";
 
 async function main() {
-    MinaNFT.minaInit('berkeley');
-    const ownerPrivateKey = PrivateKey.random();
-    const ownerPublicKey = ownerPrivateKey.toPublicKey();
-    const owner = Poseidon.hash(ownerPublicKey.toFields());
-    const pinataJWT = PINATA_JWT;
-    const deployer = PrivateKey.fromBase58(DEPLOYER);
+  const keys = MinaNFT.minaInit("local");
+  const deployer = keys ? keys[0].privateKey : PrivateKey.fromBase58(DEPLOYER);
+  const oraclePrivateKey = PrivateKey.fromBase58(NAMES_ORACLE_SK);
+  const nameServiceAddress = PublicKey.fromBase58(
+    "B62qpiD9ZWPi1JCx7hd4XcRujM1qc5jCADhhJVzTm3zZBWBpyRr3NFT"
+  );
+  const ownerPrivateKey = PrivateKey.random();
+  const ownerPublicKey = ownerPrivateKey.toPublicKey();
+  const owner = Poseidon.hash(ownerPublicKey.toFields());
+  const pinataJWT = ""; //PINATA_JWT;
 
-    const nft = new MinaNFT(`@test`);
-    nft.updateText({
-      key: `description`,
-      text: "This is my long description of the NFT. Can be of any length, supports markdown.",
-    });
-    nft.update({ key: `twitter`, value: `@builder` });
-    nft.update({ key: `secret`, value: `mysecretvalue`, isPrivate: true });
+  console.log(
+    `Deployer balance: ${await accountBalanceMina(deployer.toPublicKey())}`
+  );
 
-    await nft.updateImage({
-      filename: "./images/navigator.jpg",
-      pinataJWT,
-    });
+  const nft = new MinaNFT(`@test`);
+  nft.updateText({
+    key: `description`,
+    text: "This is my long description of the NFT. Can be of any length, supports markdown.",
+  });
+  nft.update({ key: `twitter`, value: `@builder` });
+  nft.update({ key: `secret`, value: `mysecretvalue`, isPrivate: true });
 
-    const map = new MapData();
-    map.update({ key: `level2-1`, value: `value21` });
-    map.update({ key: `level2-2`, value: `value22` });
-    map.updateText({
-      key: `level2-3`,
-      text: `This is text on level 2. Can be very long`,
-    });
+  await nft.updateImage({
+    filename: "./images/navigator.jpg",
+    pinataJWT,
+  });
 
-    await map.updateFile({
-      key: "woman",
-      filename: "./images/woman.png",
-      pinataJWT,
-    });
+  const map = new MapData();
+  map.update({ key: `level2-1`, value: `value21` });
+  map.update({ key: `level2-2`, value: `value22` });
+  map.updateText({
+    key: `level2-3`,
+    text: `This is text on level 2. Can be very long`,
+  });
 
-    const mapLevel3 = new MapData();
-    mapLevel3.update({ key: `level3-1`, value: `value31` });
-    mapLevel3.update({ key: `level3-2`, value: `value32`, isPrivate: true });
-    mapLevel3.update({ key: `level3-3`, value: `value33` });
-    map.updateMap({ key: `level2-4`, map: mapLevel3 });
-    nft.updateMap({ key: `level 2 and 3 data`, map });
+  await map.updateFile({
+    key: "woman",
+    filename: "./images/woman.png",
+    pinataJWT,
+  });
 
-    console.log(`json:`, JSON.stringify(nft.toJSON(), null, 2));
-    await MinaNFT.compile();
-    const tx = await nft.mint(deployer, owner, pinataJWT);
-    if( tx === undefined ) { throw new Error("Mint failed"); return;}
-    await MinaNFT.transactionInfo(tx);
-    console.log("Waiting for transaction to be included in a block...");
-    console.time("Transaction included in a block");
-    await MinaNFT.wait(tx);
-    console.timeEnd("Transaction included in a block");
+  const mapLevel3 = new MapData();
+  mapLevel3.update({ key: `level3-1`, value: `value31` });
+  mapLevel3.update({ key: `level3-2`, value: `value32`, isPrivate: true });
+  mapLevel3.update({ key: `level3-3`, value: `value33` });
+  map.updateMap({ key: `level2-4`, map: mapLevel3 });
+  nft.updateMap({ key: `level 2 and 3 data`, map });
+
+  console.log(`json:`, JSON.stringify(nft.toJSON(), null, 2));
+  console.log("Compiling...");
+  await MinaNFT.compile();
+
+  const nameService = new MinaNFTNameService({ oraclePrivateKey });
+  let tx = await nameService.deploy(deployer);
+  if (tx === undefined) {
+    throw new Error("Deploy failed");
+  }
+  await MinaNFT.wait(tx);
+
+  /*
+  const nameService = new MinaNFTNameService({
+    oraclePrivateKey,
+    address: nameServiceAddress,
+  });
+  */
+  tx = await nft.mint({
+    deployer,
+    owner,
+    pinataJWT,
+    nameService,
+  });
+  if (tx === undefined) {
+    throw new Error("Mint failed");
+  }
+  console.log("Waiting for transaction to be included in a block...");
+  console.time("Transaction included in a block");
+  await MinaNFT.wait(tx);
+  console.timeEnd("Transaction included in a block");
 }
 
 main().catch((error) => {
-    console.error(error);
+  console.error(error);
 });
